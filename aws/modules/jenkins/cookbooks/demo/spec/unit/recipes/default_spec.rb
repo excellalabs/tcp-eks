25999e0865b2723bc33894c30e68c7549a97e906
Craig Davies <craig.davies@excella.com>
2018-12-07 11:55:57 -0500

    Changed ubuntu to amazon in default_spec.rb

--- aws/modules/jenkins/cookbooks/demo/spec/unit/recipes/default_spec.rb
+++ aws/modules/jenkins/cookbooks/demo/spec/unit/recipes/default_spec.rb
@@ -7,11 +7,11 @@
 require 'spec_helper'
 
 describe 'demo::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When all attributes are default, on an Amazon 2018.03' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'amazon', version: '2018.03')
       runner.converge(described_recipe)
     end
 
