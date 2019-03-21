67d37c44e2b3fe7ae7c7a1167db51d60d0cb11ed
Craig Davies <craig.davies@excella.com>
2019-03-21 15:24:26 -0400

    Use jenkins from github.com/excellaco/terraform-aws-ec2-jenkins-server.git

--- aws/modules/jenkins/cookbooks/demo/recipes/jenkins-disable-authentication.rb
+++
@@ -1,8 +0,0 @@
-execute "Disable Jenkins Authentication" do
-  command "sed -i 's@<useSecurity>true</useSecurity>@<useSecurity>false</useSecurity>@g' /var/lib/jenkins/config.xml"
-end
-
-# authentication is only disabled upon restart
-service 'jenkins' do
-  action :restart
-end
--- aws/modules/jenkins/cookbooks/demo/recipes/lockable.rb
+++
@@ -1,9 +0,0 @@
-template '/var/lib/jenkins/org.jenkins.plugins.lockableresources.LockableResourcesManager.xml' do
-  source 'lockable.xml.erb'
-  owner 'jenkins'
-  group 'jenkins'
-  action :create
-  variables(
-    lockable: node['jenkins']['lockable']
-  )
-end
