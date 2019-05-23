dd7c37fbf68f520beab49ff7445efa6adee383d9
Craig Davies <craig.davies@excella.com>
2019-05-23 13:58:07 -0400

    Update AWS URL in run

--- run.sh
+++ run.sh
@@ -41,23 +41,23 @@ echo "Running [$1]"
 pushd aws &> /dev/null
 case "$1" in
   init)
-    if [ ! -d $KEY_ROOT ]; then
-      mkdir $KEY_ROOT
+    if [ ! -d ${KEY_ROOT} ]; then
+      mkdir ${KEY_ROOT}
     fi
-    if [ ! -f $KEY_ROOT/bench-tc-bastion.pub ]; then
-      echo "${BOLD}Creating SSH keys for bench-tc-bastion instance${NORMAL}"
-      ssh-keygen -t rsa -b 4096 -o -a 100 -f $KEY_ROOT/bench-tc-bastion
-      ssh-keygen -f $KEY_ROOT/bench-tc-bastion.pub -m pem -e > $KEY_ROOT/bench-tc-bastion.pem
+    if [ ! -f ${KEY_ROOT}/${PROJECT_NAME}-bastion.pub ]; then
+      echo "Creating SSH keys for ${PROJECT_NAME}-bastion instance"
+      ssh-keygen -t rsa -b 4096 -o -a 100 -f ${KEY_ROOT}/${PROJECT_NAME}-bastion
+      ssh-keygen -f ${KEY_ROOT}/${PROJECT_NAME}-bastion.pub -m pem -e > ${KEY_ROOT}/${PROJECT_NAME}-bastion.pem
     fi
-    if [ ! -f $KEY_ROOT/bench-tc-cluster.pub ]; then
-      echo "${BOLD}Creating SSH keys for bench-tc-cluster instances${NORMAL}"
-      ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f $KEY_ROOT/bench-tc-cluster
-      ssh-keygen -f $KEY_ROOT/bench-tc-cluster.pub -m pem -e > $KEY_ROOT/bench-tc-cluster.pem
+    if [ ! -f ${KEY_ROOT}/${PROJECT_NAME}-cluster.pub ]; then
+      echo "Creating SSH keys for ${PROJECT_NAME}-cluster instances"
+      ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f ${KEY_ROOT}/${PROJECT_NAME}-cluster
+      ssh-keygen -f ${KEY_ROOT}/${PROJECT_NAME}-cluster.pub -m pem -e > ${KEY_ROOT}/${PROJECT_NAME}-cluster.pem
     fi
-    if [ ! -f $KEY_ROOT/bench-tc-jenkins.pub ]; then
-      echo "${BOLD}Creating SSH keys for Jenkins instance${NORMAL}"
-      ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f $KEY_ROOT/bench-tc-jenkins
-      ssh-keygen -f $KEY_ROOT/bench-tc-jenkins.pub -m pem -e > $KEY_ROOT/bench-tc-jenkins.pem
+    if [ ! -f ${KEY_ROOT}/${PROJECT_NAME}-jenkins.pub ]; then
+      echo "Creating SSH keys for ${PROJECT_NAME}-jenkins instance"
+      ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f ${KEY_ROOT}/${PROJECT_NAME}-jenkins
+      ssh-keygen -f ${KEY_ROOT}/${PROJECT_NAME}-jenkins.pub -m pem -e > ${KEY_ROOT}/${PROJECT_NAME}-jenkins.pem
     fi
     terraform init -backend-config="bucket=${PROJECT_NAME}-${ENVIRONMENT}" -backend-config="key=terraform.tfstate" -backend-config="region=${AWS_REGION}" -backend-config="encrypt=true"
     ;;
@@ -69,9 +69,9 @@ case "$1" in
     ;;
   all)
     popd &> /dev/null
-	curl -s -qL -o terraform.zip https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_darwin_amd64.zip
-	unzip -o terraform.zip
-	chmod +x ./terraform
+    curl -s -qL -o terraform.zip https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_darwin_amd64.zip
+    unzip -o terraform.zip
+    chmod +x ./terraform
     eval $0 init
     eval $0 plan
     eval $0 apply
@@ -80,17 +80,16 @@ case "$1" in
     terraform destroy 
     ;;
   chef)
-    pushd modules/jenkins-master
+    pushd modules/jenkins
     berks package ./cookbooks.tar.gz --berksfile=./cookbooks/demo/Berksfile
     popd
-    scp -i ../keys/jenkins modules/jenkins-master/cookbooks.tar.gz  ubuntu@$(terraform output jenkins_master_public_dns):/tmp/
-    ssh -i ../keys/jenkins ubuntu@$(terraform output jenkins_master_public_dns) sudo chef-solo --recipe-url /tmp/cookbooks.tar.gz -j /tmp/chef.json
+    scp -i ../keys/jenkins modules/jenkins/cookbooks.tar.gz ubuntu@$(terraform output jenkins_master_public_dns):/tmp/
+    ssh -i ../keys/jenkins ubuntu@$(terraform output jenkins_public_dns) sudo chef-solo --recipe-url /tmp/cookbooks.tar.gz -j /tmp/chef.json
     ;;
   kube)
-    curl -s -qL -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.11/2018-12-06/bin/darwin/amd64/kubectl
-    curl -s -qL -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.11/2018-12-06/bin/darwin/amd64/aws-iam-authenticator
-    chmod +x ./kubectl
-    chmod +x ./aws-iam-authenticator
+    curl -s -qL -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.13/2019-03-13/bin/darwin/amd64/kubectl
+    curl -s -qL -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.13/2019-03-13/bin/darwin/amd64/aws-iam-authenticator
+    chmod +x ./kubectl ./aws-iam-authenticator
     ;;
   *)
     error_and_exit "$USAGE"
